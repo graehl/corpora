5872b55d1c55b3d53d8c030d557b2bfbb2a7c1f3
graehl <graehl@gmail.com>
2019-10-21 01:14:27 -0700

    wp

--- wiki/wiki-first.sh
+++ wiki/wiki-first.sh
@@ -1,4 +1,6 @@
 n=${1:-100000}
 o=wiki-first.$n.txt
-[[ -f $o ]] || `dirname $0`/head-wikitext.sh $((n/50)) --minlen 80 --first True | head -n $n > $o.tmp && mv $o.tmp $o
+if ! [[ -f $o ]]; then
+    `dirname $0`/head-wikitext.sh $((n/50)) --minlen 80 --first True | head -n $n > $o.tmp && mv $o.tmp $o
+fi
 echo $o
