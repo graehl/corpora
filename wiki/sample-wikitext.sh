ac4c412f9e54b5cfb286e475d03493ab9a34746b
graehl <graehl@gmail.com>
2019-10-20 17:44:39 -0700

    wp

--- wiki/sample-wikitext.sh
+++ wiki/sample-wikitext.sh
@@ -4,4 +4,4 @@ bytes=${bytes:-$((mb*1000000))}
 range=0-${bytes}
 first=True
 minlen=80
-curl -r$range https://www.cs.upc.edu/~nlp/wikicorpus/raw.en.tgz | tar xOzf - | python ./filter-wikitext.py --first $first --minlen $minlen | wp.${mb}mb.$first.txt
+curl -r$range https://www.cs.upc.edu/~nlp/wikicorpus/raw.en.tgz | tar xOzf - | python ./filter-wikitext.py --first $first --minlen $minlen > wp.${mb}mb.$first.txt
