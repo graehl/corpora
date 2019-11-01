5885fd812249203a6c1b372dff85d102e69fe380
graehl <graehl@gmail.com>
2019-11-01 15:07:44 -0700

    semeval17-4-2 financial headline sentiment

---
+++ sentiment/semfin.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+import plac
+import json
+
+@plac.annotations(
+    ntrain=("max training lines", "option", "t", int),
+    ndev=("max dev lines if >0", "option", "d", int),
+    pos=("threshold on [-1,1] at or above which we say positive (class 1)", "option", "p", float),
+    neg=("threshold on [-1,1] below which we say negative (class 0)", "option", "n", float),
+    )
+def main(ntrain=1000, ndev=-1, pos=0.3, neg=-.3):
+    n = 0
+    data = json.load(sys.stdin)
+    outfn = 'train.tsv'
+    outf = open(outfn, 'w', encoding='utf-8')
+    for ts in data:
+        if n < ntrain:
+            n += 1
+        else:
+            if n == ntrain:
+                n += 1
+                outfn = 'dev.tsv'
+                outf = open(outfn, 'w', encoding='utf-8')
+            if ndev > 0: ndev -= 1
+            if ndev == 0: break
+        text = ts["title"]
+        s = float(ts["sentiment"])
+        nlabel = '1' if s >= pos else '0' if s < neg else '2'
+        outf.write('%s\t%s\n' % (text, nlabel))
+
+if __name__ == "__main__":
+    plac.call(main)
---
+++ sentiment/semfin.sh
@@ -0,0 +1,8 @@
+export PYTHONIOENCODING=utf-8
+[[ -d semfin2 ]] || git clone https://bitbucket.org/ssix-project/semeval-2017-task-5-subtask-2.git semfin2
+#git clone https://bitbucket.org/ssix-project/semeval-2017-task-5-subtask-1.git
+cd semfin2
+set -x
+python ../semfin.py --ntrain=1000 --pos=0.3 --neg=-.03 < Headline_Trainingdata.json
+wc -l *.tsv
+head *.tsv
